92a8e77c05bcb740ba3fd68f8a11ac1eaeb3b18e
Kevin Sullivan <sideslapd@gmail.com>
2014-10-13 17:21:02 -0400

    Bye Biff
    Got rid of the first project that was falling to pieces as I tried to
    duct tape objective-c and swift without really knowing what the hell I
    was doing.

--- SeaFoamClientFucked/Pods/Target Support Files/Pods-SeaFoamClient-CocoaAsyncSocket/Pods-SeaFoamClient-CocoaAsyncSocket-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SeaFoamClient_CocoaAsyncSocket : NSObject
-@end
-@implementation PodsDummy_Pods_SeaFoamClient_CocoaAsyncSocket
-@end
--- SeaFoamClientFucked/SeaFoamClient/Pods/Target Support Files/Pods-SeaFoamClient/Pods-SeaFoamClient-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SeaFoamClient : NSObject
-@end
-@implementation PodsDummy_Pods_SeaFoamClient
-@end
