422a93878c473c27a09961fa1563272391e541f8
Kevin Sullivan <sideslapd@gmail.com>
2014-10-14 15:49:52 -0400

    Login
    Login is working for the following cases: correct username/pass, wrong
    pass, nonexistent username. None of these cases are handled in any way
    but a verbose message is printed on the server and sent to the (test)
    client.

--- simpleSocketClient.py
+++ simpleSocketClient.py
@@ -1,24 +1,53 @@
 import socket 
 
+# MAIN TODO THINKER: I'm pretty sure (but not 100%) that connections should
+# be retained rather than connect/close every message... It could work
+# this way but all of the calls will be synchronous (unless we use clever threading)
+
 # Client configuration
 host = 'localhost' 
-port = 5008
+port = 5013
 size = 4096
 
-# Create the socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+# Send a message and print + return the response
+def verboseSend(message):
+	# Create the socket
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
-# Connect to the specified socket
-s.connect((host,port)) 
+	# Connect to the new socket
+	s.connect((host,port)) 
 
-# Send some text data
-s.send('LOGIN - Kevin:test')
+	# Send the message
+	print 'Sending: ' + message
+	s.send(message)
 
-# Wait for a response and store it (size = buffer) 
-data = s.recv(size) 
+	# Receive the response
+	data = s.recv(size)
+	print 'Received: ' + data
+	print
 
-# Close the socket
-s.close() 
+	# Close the socket
+	s.close()
 
-# Display result
-print 'Received:', data
+	# Return the response
+	return data
+
+# Test all login possibilities
+def testLogin():
+	if verboseSend('LOGIN - Kevin:test') == None:
+		return False
+	if verboseSend('LOGIN - Kevin:stupid') == None:
+		return False
+	if verboseSend('LOGIN - Riley:super') == None:
+		return False
+
+	return True
+
+# Main function separate so this can be modulized	
+def main():
+	if testLogin():
+		print 'We passed all login tests, great job!'
+
+# Main function separate so this can run as a module
+if __name__ == "__main__":
+    main()
--- simpleSocketServer.py
+++ simpleSocketServer.py
@@ -1,58 +1,97 @@
 import socket  
 from pymongo import MongoClient
 
-# Server configuration
+# Global Server configuration
 host = 'localhost' 
-port = 5008
+port = 5013
 backlog = 5 
 size = 4096 
 clients = []
 addresses = []
 
-# Get a handle on our MongoDB database
-dbClient = MongoClient('mongodb://seafoam:seafoam@ds041140.mongolab.com:41140/seafoam')
-db = dbClient.seafoam
-users = db.users
+# Converts a cursor object (returned from MongoDB request)
+# into a list by iterating over it (not super efficient)
+def queryToList(cursorObject):
+	myList = []
 
-# Create the socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+	for document in cursorObject:
+		myList.append(document)
 
-# Bind the socket and begin listening
-s.bind((host,port)) 
-s.listen(backlog) 
+	return myList
 
-# Listen indefinitely
-while 1: 
-	# Accept an incoming connection, store the client socket and the conn address
-	accepted = s.accept()
-	clients.append(accepted[0])		# Socket
-	addresses.append(accepted[1])	# Address
-    
-    # Receive the data (size = buffer)
-	data = clients[-1].recv(size) 
+# Prints a simple string along with address data
+def printInfo(message):
+	print 'Address: ' + addresses[-1][0] + ':' + addresses[-1][1]
+	print message + '\n'
 
-    # Make sure the data was received properly
-	if data: 
-		if data[0:5] == 'LOGIN':
-			username, password = data[8:].split(':')
-			print 'Username: ' + username
-			print 'Password: ' + password
+def main():
+	# Get a handle on our MongoDB database
+	dbClient = MongoClient('mongodb://seafoam:seafoam@ds041140.mongolab.com:41140/seafoam')
+	db = dbClient.seafoam
+	users = db.users
 
-			currentUser = users.find( {u'password' : password} )
+	# Create the socket
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
-			
+	# Bind the socket and begin listening
+	s.bind((host,port)) 
+	s.listen(backlog) 
 
-			for password in allPasswords:
-				print password
+	# Listen indefinitely
+	while 1: 
+		# Accept an incoming connection, store the client socket and the conn address
+		accepted = s.accept()
+		clients.append(accepted[0])		
+		addresses.append((str(accepted[1][0]), str(accepted[1][1])))
+	    # TODO I converted to string to save work later - might not wanna
 
-			print users.find_one()
+	    # Receive the data (size = buffer)
+		data = clients[-1].recv(size) 
 
+	    # Make sure the data was received properly
+		if data: 
+			# If we've found the login tag...
+			if data[0:5] == 'LOGIN':
+				# Extract the username and password
+				username, password = data[8:].split(':')
+				print 'Username: ' + username
+				print 'Password: ' + password
 
-    	# Echo it back to the client
-		print 'Oooo I got: ' + data
-		clients[-1].send(data) 
+				# Query the database for the provide username/password combo
+				dbResponse = queryToList(users.find({ 'username' : username, 'password' : password }))
+				clientResponse = ''
 
-    # Close the current connection
-	clients[-1].close()
+				# We received multiple responses - this should be possible
+				if len(dbResponse) > 1:
+					clientResponse = 'ERROR - We received multiple results for the username ' + username + \
+					'this should not be possible unless user was added manually'
 
-	#'mongodb://seafoam:seafoam@ds041140.mongolab.com:41140/seafoam')
+					printInfo(clientResponse)
+				# We didn't receive any responses...
+				elif len(dbResponse) == 0:
+					# Check the database for that username (forgotten password)
+					# TODO - prompt the user on account creation with that userna,e
+					if users.find_one({ 'username': username }) == None:
+						clientResponse = 'No user found for username (create option?) ' + username
+					else:
+						clientResponse = 'Incorrect password for username ' + username
+
+					printInfo(clientResponse)
+				# This is the result we expect - it indicates a successful login
+				elif len(dbResponse) == 1:
+					clientResponse = 'You just got LOGGED THE FUCK ON'
+					printInfo(clientResponse)
+				# This happens if the cursor object has negative documents - probably impossible
+				else:
+					clientResponse = 'ERROR: This statement should be impossible to reach'
+					printInfo(clientResponse)
+
+	    	# Respond to the client
+			clients[-1].send(clientResponse) 
+
+	    # Close the current connection
+		clients[-1].close()
+
+# Main function separate so this can run as a module
+if __name__ == "__main__":
+    main()
