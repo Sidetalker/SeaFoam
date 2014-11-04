f8aa0d67483ecb0825faeb412d0e1e39ad9140ac
Kevin Sullivan <sideslapd@gmail.com>
2014-11-04 12:41:47 -0500

    Progression
    Haven’t committed in a while - no earth shattering changes

--- SeaFoamClient-iOS/SeaFoamClient/MainTableViewController.swift
+++ SeaFoamClient-iOS/SeaFoamClient/MainTableViewController.swift
@@ -22,8 +22,14 @@ class MainTableViewController: UITableViewController, SeaSocketDelegate {
     var myFoam: SeaSocket?
     var userID: String?
     
+    // Chatroom information
     var chats: [chatInfo]?
     var chatsTest: Array<String>?
+    
+    // UITableViewCell elements
+    @IBOutlet var starImage: UIImageView!
+    @IBOutlet var cellLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,10 +69,15 @@ class MainTableViewController: UITableViewController, SeaSocketDelegate {
                 self.myFoam?.createChat(messageTextField.text, userID: self.userID!)
             }
             else {
-                jiggle(messageTextField)
+                messageDisplay.message = "You must enter a name for your chatroom"
                 self.presentViewController(messageDisplay, animated: true, completion: nil)
             }
         }))
+        
+        messageDisplay.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: {
+            (alert: UIAlertAction!) in
+        }))
+        
         self.presentViewController(messageDisplay, animated: true, completion: nil)
     }
     
@@ -91,51 +102,6 @@ class MainTableViewController: UITableViewController, SeaSocketDelegate {
 
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     // MARK: - SeaSocket Delegates
     
